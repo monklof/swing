0fdee3d772387fb0edf73cd5a680e86abaf3f15c
liuchao12 <liuchao12@meituan.com>
2016-04-24 10:58:23 +0800

    update release version

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name = "swing",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "monklof",
     author_email = "monklof@gmail.com",
     description = ("Configuration switcher by an environment variable inspired by Common Lisp's envy"),
